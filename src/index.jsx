39917c79e3804d27d737150225a86e0095dc3a62
Dmytro Donchenko <drmegman@gmail.com>
2023-09-11 23:04:37 +0300

    File changes for gh-pages deployment

--- src/index.jsx
+++ src/index.jsx
@@ -1,14 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 import "normalize.css/normalize.css";
 import "./styles/index.scss";
-import 'swiper/scss';
-import 'swiper/scss/navigation';
-import 'swiper/scss/pagination';
-import 'swiper/scss/autoplay';
-import App from './app/App';
+import "swiper/scss";
+import "swiper/scss/navigation";
+import "swiper/scss/pagination";
+import "swiper/scss/autoplay";
+import App from "./app/App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
